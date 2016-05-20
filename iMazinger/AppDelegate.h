ea2642ca311bef5abe696677849f5987056d1f4a
David de Tena <daviddetenacalvo@gmail.com>
2016-05-20 09:47:58 +0200

    Initial Commit

---
+++ iMazinger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iMazinger
+//
+//  Created by David de Tena on 20/05/16.
+//  Copyright © 2016 David de Tena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
